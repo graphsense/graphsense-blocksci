b03b2518baade0c1827637887cc96b84c87f2ba6
Rainer Stuetz <rainer.stuetz@ait.ac.at>
2018-11-26 10:37:56 +0100

    Remove update script

--- blocksci_update.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 CONTAINER_NAME"
-    exit 1
-fi
-
-docker exec -ti "$1" blocksci_parser --output-directory /var/data/blocksci_data update --max-block -6 disk --coin-directory /var/data/block_data

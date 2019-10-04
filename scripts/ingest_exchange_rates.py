df900cc30b22ac4409e8db4a8dafdf5c2bd947dc
Rainer Stuetz <rainer.stuetz@ait.ac.at>
2019-10-04 13:42:15 +0200

    Polish code

--- scripts/ingest_exchange_rates.py
+++ scripts/ingest_exchange_rates.py
@@ -1,8 +1,9 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # coding: utf-8
+'''Script to fetch cryptocurrency exchange rates from CoinMarketCap'''
 
 from argparse import ArgumentParser
-import datetime
+from datetime import date, datetime, timedelta
 
 import bs4
 from cassandra.cluster import Cluster
@@ -18,27 +19,22 @@ class ExchangeRateParsingError(Exception):
 def fx_rates_url(start, end, base, *symbols):
     symbols = [symbol.upper() for symbol in symbols]
     symbols = ','.join(symbols)
-    return "https://api.exchangeratesapi.io/history?" \
-           "start_at={start}&end_at={end}&symbols={symbols}&base={base}" \
-           .format(start=start, end=end, symbols=symbols, base=base)
+    return 'https://api.exchangeratesapi.io/history?' \
+           f'start_at={start}&end_at={end}&symbols={symbols}&base={base}'
 
 
 def historical_coin_url(slug, start, end):
-    return "https://coinmarketcap.com/currencies/{slug}/historical-data/?" \
-           "start={start}&end={end}" \
-           .format(slug=slug,
-                   start=start.replace('-', ''), end=end.replace('-', ''))
-
-
-def all_url():
-    return 'https://coinmarketcap.com/all/views/all/'
+    start = start.replace('-', '')
+    end = end.replace('-', '')
+    return f'https://coinmarketcap.com/currencies/{slug}/historical-data/' + \
+           f'?start={start}&end={end}'
 
 
 def parse_all_response(resp):
     soup = bs4.BeautifulSoup(resp.text, 'lxml')
     table = soup.find('table')
-    columns = ['slug'] + [x.get('id', 'th-#')[3:]
-                          for x in table.thead.find_all('th')]
+    columns = ['slug'] + \
+              [x.get('id', 'th-#')[3:] for x in table.thead.find_all('th')]
 
     def get_val(td):
         tag = td
@@ -46,8 +42,7 @@ def parse_all_response(resp):
         if tag.find('a'):
             tag = tag.find('a')
         # numeric columns store their value in these attributes in addition
-        # to text.
-        # use these attributes to avoid parsing $ and , in text
+        # to text. use these attributes to avoid parsing $ and , in text
         for key in ['data-usd', 'data-supply']:
             val = tag.get(key)
             if val:
@@ -112,8 +107,8 @@ def lookup_slug(all_df, symbol):
     if df_row.empty:
         return None
     slug = df_row['slug'].tolist()
-    if(len(slug) > 1):
-        raise ExchangeRateParsingError("Found more than one possible slugs")
+    if len(slug) > 1:
+        raise ExchangeRateParsingError('Found more than one possible slugs')
     return slug[0]
 
 
@@ -122,15 +117,15 @@ def query_required_currencies(session, keyspace, table):
         return pd.DataFrame(rows, columns=colnames)
     session.row_factory = pandas_factory
 
-    query = f"""SELECT column_name FROM system_schema.columns
+    query = f'''SELECT column_name FROM system_schema.columns
                 WHERE keyspace_name = '{keyspace}'
-                AND table_name = '{table}';"""
+                AND table_name = '{table}';'''
     result = session.execute(query)
     df = result._current_rows
     currencies = [symbol.upper() for symbol in list(df['column_name'])]
     if 'USD' not in currencies or 'DATE' not in currencies:
         raise ExchangeRateParsingError(
-            "USD is mandatory and must be defined in the schema.")
+            'USD is mandatory and must be defined in the schema.')
     currencies.remove('DATE')
     currencies.remove('USD')
     return currencies
@@ -141,24 +136,27 @@ def query_most_recent_date(session, keyspace, table):
         return pd.DataFrame(rows, columns=colnames)
     session.row_factory = pandas_factory
 
-    query = f"""SELECT date FROM {keyspace}.{table};"""
+    query = f'''SELECT date FROM {keyspace}.{table};'''
 
     result = session.execute(query)
     df = result._current_rows
-    if len(df) == 0:
+    if df.empty:
         return None
     df['date'] = df['date'].astype('datetime64')
 
     largest = df.nlargest(1, 'date').iloc[0]['date']
 
-    return largest.strftime("%Y-%m-%d")
+    return largest.strftime('%Y-%m-%d')
 
 
 def fetch_crypto_exchange_rates(start, end, crypto_currency):
-    all_crypto_df = parse_all_response(requests.get(all_url()))
+    '''Fetch most recent entry from exchange rates table.'''
+
+    all_url = 'https://coinmarketcap.com/all/views/all/'
+    all_crypto_df = parse_all_response(requests.get(all_url))
     slug = lookup_slug(all_crypto_df, crypto_currency)
     crypto_url = historical_coin_url(slug, start, end)
-    print(f"Fetching {crypto_currency} exchange rates from {crypto_url}")
+    print(f'Fetching {crypto_currency} exchange rates from {crypto_url}')
     crypto_resp = requests.get(crypto_url)
     crypto_df = parse_historical_coin_response(crypto_resp)
     crypto_df = crypto_df[['date', 'close']].rename(columns={'close': 'USD'})
@@ -166,104 +164,107 @@ def fetch_crypto_exchange_rates(start, end, crypto_currency):
 
 
 def insert_exchange_rates(session, keyspace, table, exchange_rates_df):
+    '''Insert exchange rates into Cassandra table.'''
+
     colnames = ','.join(exchange_rates_df.columns)
     values = ','.join(['?' for i in range(len(exchange_rates_df.columns))])
-    query = f"""INSERT INTO {keyspace}.{table}({colnames})
-                VALUES ({values})"""
+    query = f'''INSERT INTO {keyspace}.{table}({colnames}) VALUES ({values})'''
     prepared = session.prepare(query)
 
-    for index, row in exchange_rates_df.iterrows():
+    for _, row in exchange_rates_df.iterrows():
         session.execute(prepared, row)
 
 
 def main():
+    '''Main function.'''
+
+    MIN_START = '2009-01-01'
+    prev_date = (date.today() - timedelta(days=1)).strftime('%Y-%m-%d')
+
     parser = ArgumentParser(description='Ingest exchange rates into Cassandra',
                             epilog='GraphSense - http://graphsense.info')
     parser.add_argument('-d', '--db_nodes', dest='db_nodes', nargs='+',
                         default=['localhost'], metavar='DB_NODE',
-                        help='list of Cassandra nodes; default "localhost")')
+                        help='list of Cassandra nodes; default "localhost"')
+    parser.add_argument('-f', '--force', dest='force', action='store_true',
+                        help='do not fetch most recent entries from '
+                             'Cassandra and overwrite existing records')
     parser.add_argument('-k', '--keyspace', dest='keyspace',
                         required=True,
                         help='Cassandra keyspace')
     parser.add_argument('-t', '--table', dest='table',
                         default='exchange_rates',
-                        help='Name of the target echange rate table')
-    parser.add_argument('--start_date', dest='start',
-                        type=str, default='2009-01-01',
+                        help='name of the target exchange rate table')
+    parser.add_argument('--start_date', dest='start', type=str,
+                        default=MIN_START,
                         help='start date for fetching exchange rates')
-    parser.add_argument('--end_date', dest='end',
-                        type=str, default=datetime.date.today()
-                                                       .strftime("%Y-%m-%d"),
+    parser.add_argument('--end_date', dest='end', type=str,
+                        default=prev_date,
                         help='end date for fetching exchange rates')
     parser.add_argument('-c', '--cryptocurrency', dest='cryptocurrency',
-                        type=str, default='BTC',
-                        required=True,
-                        help='the target cryptocurrency')
+                        type=str, default='BTC', required=True,
+                        help='target cryptocurrency')
 
     args = parser.parse_args()
 
     cluster = Cluster(args.db_nodes)
     keyspace = args.keyspace
     table = args.table
-
     session = cluster.connect(keyspace)
+
+    crypto_currency = args.cryptocurrency
 
     # Default start and end date
     start = args.start
     end = args.end
 
-    crypto_currency = args.cryptocurrency
+    print(f'*** Starting exchange rate ingest for {crypto_currency} ***')
 
-    print(f"*** Starting exchange rate ingest for {crypto_currency} ***")
+    if datetime.fromisoformat(start) < datetime.fromisoformat(MIN_START):
+        start = MIN_START
 
-    # Query most recent data in 'exchange_rates' table
-    try:
+    # query most recent data
+    if not args.force:
         most_recent_date = query_most_recent_date(session, keyspace, table)
         if most_recent_date is not None:
             start = most_recent_date
-        print(f"Start date: {start}")
-        print(f"End date: {end}")
-    except ExchangeRateParsingError as err:
-        print(f"Error while querying most recent date: {err}")
 
-    # Query all required fiat currencies from the 'exchange_rates' table
+    print(f'Start date: {start}')
+    print(f'End date: {end}')
+
+    if datetime.fromisoformat(start) > datetime.fromisoformat(end):
+        print('Error: start date after end date.')
+        cluster.shutdown()
+        raise SystemExit
+
+    # query all required fiat currencies
     try:
         fiat_currencies = query_required_currencies(session, keyspace, table)
-        print(f"Target fiat currencies: {fiat_currencies}")
+        print(f'Target fiat currencies: {fiat_currencies}')
     except ExchangeRateParsingError as err:
-        print(f"Error while querying all required fiat currencies: {err}")
+        print(f'Error while querying all required fiat currencies: {err}')
 
-    # Fetch crypto currency exchange rates in USD
-    try:
-        crypto_df = fetch_crypto_exchange_rates(start, end, crypto_currency)
-    except ExchangeRateParsingError as err:
-        print(f"Error while fetching exchange rates in USD: {err}")
+    # fetch crypto currency exchange rates in USD
+    crypto_df = fetch_crypto_exchange_rates(start, end, crypto_currency)
 
-    # Query conversion rates and merge converted values in exchange rates
-    try:
-        exchange_rates = crypto_df
-        for fiat_currency in fiat_currencies:
-            url = fx_rates_url(start, end, "USD", fiat_currency)
-            print(f"Fetching conversion rates for {fiat_currency} from {url}")
-            fx_resp = requests.get(url)
-            fx_df = parse_fx_rates_response(fx_resp)
-            merged_df = crypto_df.merge(fx_df, how="left", on='date')
-            merged_df['fx_rate'] = merged_df['fx_rate'] \
-                .interpolate(method='linear')
-            merged_df['fx_rate'] = merged_df['fx_rate'].fillna(method='ffill')
-            merged_df['fx_rate'] = merged_df['fx_rate'].fillna(method='bfill')
-            merged_df[fiat_currency] = merged_df['USD'] * merged_df['fx_rate']
-            merged_df = merged_df[['date', fiat_currency]]
-            exchange_rates = exchange_rates.merge(merged_df, on='date')
-    except ExchangeRateParsingError as err:
-        print(f"Error while querying conversion rates: {err}")
+    # query conversion rates and merge converted values in exchange rates
+    exchange_rates = crypto_df
+    for fiat_currency in fiat_currencies:
+        url = fx_rates_url(start, end, 'USD', fiat_currency)
+        print(f'Fetching conversion rates for {fiat_currency} from {url}')
+        fx_resp = requests.get(url)
+        fx_df = parse_fx_rates_response(fx_resp)
+        merged_df = crypto_df.merge(fx_df, how='left', on='date')
+        merged_df['fx_rate'].interpolate(method='linear', inplace=True)
+        merged_df['fx_rate'].fillna(method='ffill', inplace=True)
+        merged_df['fx_rate'].fillna(method='bfill', inplace=True)
+        merged_df[fiat_currency] = merged_df['USD'] * merged_df['fx_rate']
+        merged_df = merged_df[['date', fiat_currency]]
+        exchange_rates = exchange_rates.merge(merged_df, on='date')
 
-    # Insert final exchange rates into Cassandra 'exchange rates' table
-    try:
-        print(f"Inserted rates for {len(exchange_rates)} days")
-        insert_exchange_rates(session, keyspace, table, exchange_rates)
-    except ExchangeRateParsingError as err:
-        print(f"Error while inserting exchange rates into Cassandra: {err}")
+    # insert final exchange rates into Cassandra
+    print(f'Inserted rates for {len(exchange_rates)} days')
+    insert_exchange_rates(session, keyspace, table, exchange_rates)
 
     cluster.shutdown()
 

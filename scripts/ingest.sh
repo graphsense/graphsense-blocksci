85316e039433910a334bf1faf038da154c38d53b
Rainer Stuetz <rainer.stuetz@ait.ac.at>
2020-11-03 09:41:26 +0100

    Fix environment variable

--- scripts/ingest.sh
+++ scripts/ingest.sh
@@ -2,7 +2,7 @@
 python3 -u /usr/local/bin/blocksci_export.py \
     -c /opt/graphsense/blocksci.cfg \
     -d $CASSANDRA_HOST \
-    -k RAW_KEYSPACE \
+    -k $RAW_KEYSPACE \
     --processes $PROCESSES \
     --continue \
     --previous_day

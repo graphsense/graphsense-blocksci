4ddf00225ff60e4a82697338d4863557dbdfa399
Rainer Stuetz <rainer.stuetz@ait.ac.at>
2022-03-15 16:00:58 +0100

    Improve ingest script

--- scripts/ingest.sh
+++ scripts/ingest.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 python3 -u /usr/local/bin/blocksci_export.py \
     --config /opt/graphsense/blocksci.cfg \
-    --db_nodes $CASSANDRA_HOST \
-    --keyspace $RAW_KEYSPACE \
-    --processes $PROCESSES \
+    --db_nodes "$CASSANDRA_HOST" \
+    --keyspace "$RAW_KEYSPACE" \
+    --processes "$PROCESSES" \
     --continue \
     --previous_day

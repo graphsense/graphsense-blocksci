9131382ea83180e2c9ae04fd1752353f63d124ac
Rainer Stuetz <rainer.stuetz@ait.ac.at>
2022-03-15 15:59:48 +0100

    Update ingest script

--- scripts/ingest.sh
+++ scripts/ingest.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 python3 -u /usr/local/bin/blocksci_export.py \
-    -c /opt/graphsense/blocksci.cfg \
-    -d $CASSANDRA_HOST \
-    -k $RAW_KEYSPACE \
+    --config /opt/graphsense/blocksci.cfg \
+    --db_nodes $CASSANDRA_HOST \
+    --keyspace $RAW_KEYSPACE \
     --processes $PROCESSES \
     --continue \
     --previous_day

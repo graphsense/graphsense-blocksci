70f3789529f9b7e630fd66ac8efe7f2e62795a67
Rainer Stuetz <rainer.stuetz@ait.ac.at>
2022-03-16 09:55:55 +0100

    Change command-line arguments

--- scripts/ingest.sh
+++ scripts/ingest.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 python3 -u /usr/local/bin/blocksci_export.py \
     --config /opt/graphsense/blocksci.cfg \
-    --db_nodes "$CASSANDRA_HOST" \
-    --keyspace "$RAW_KEYSPACE" \
+    --db-nodes "$CASSANDRA_HOST" \
+    --db-keyspace "$RAW_KEYSPACE" \
     --processes "$PROCESSES" \
     --continue \
-    --previous_day
+    --previous-day
